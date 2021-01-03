924487ea6c296d4dfe32ea7646cee5a80c773bdc
mahdi <mahdisamimi.m@gmail.com>
2021-01-04 01:11:26 +0330

    dynamicStyle

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from "react";
 import Persons from "./components/person/persons";
-import Person from './components/person/Person';
+import "../node_modules/font-awesome/css/font-awesome.min.css";
+import { ToastContainer, toast } from "react-toastify";
+// import Person from "./components/person/Person";
 class App extends Component {
   state = {
-    persons: [
-      { id: 1, fullname: "حسین محمدی" },
-      { id: 2, fullname: "یاسر زمانیان" },
-      { id: 3, fullname: " منصور نقی زاده" },
-    ],
-
-    showPersons: false,
+    persons: [],
+    person: "",
+    showPersons: true,
   };
 
   handleShowPerson = () => {
@@ -20,52 +18,116 @@ class App extends Component {
     const persons = [...this.state.persons];
     const filteredPersons = persons.filter((p) => p.id !== id);
     this.setState({ persons: filteredPersons });
+
+    const personIndex = persons.findIndex((p) => p.id === id);
+    const person = persons[personIndex];
+
+    toast.error(`${person.fullname} با موفقیت حذف شد ,`, {
+      position: "bottom-left",
+      closeOnClick: true,
+    });
   };
-  handleNameChange= (event,id) =>{
-    const {persons :allPersons} =this.state;
+  handleNameChange = (event, id) => {
+    const { persons: allPersons } = this.state;
 
-    const personIndex=allPersons.findIndex(p => p.id === id);
-    const person=allPersons[personIndex];
-    person.fullname=event.target.value;
+    const personIndex = allPersons.findIndex((p) => p.id === id);
+    const person = allPersons[personIndex];
+    person.fullname = event.target.value;
 
-    const persons=[...allPersons];
-    persons[personIndex]=person;
-    this.setState({persons})
+    const persons = [...allPersons];
+    persons[personIndex] = person;
+    this.setState({ persons });
+  };
 
+  handleNewPerson = () => {
+    const persons = [...this.state.persons];
+    const person = {
+      id: Math.floor(Math.random() * 1000),
+      fullname: this.state.person
+    };
+    if (person.fullname !== "" && person.fullname !== " ") {
+      persons.push(person);
+      this.setState({ persons, person: "" });
+      toast.success("شخص جدید اضافه شد .", {
+        position: "bottom-right",
+        closeButton: true,
+        closeOnClick: true,
+      });
+    }
+  };
 
-  }
+  setPerson = (event) => {
+    this.setState({ person: event.target.value });
+  };
 
   render() {
     const { persons, showPersons } = this.state;
-    const styles = {
-      textAlign: "center",
-    };
-
-    const buttonStyles = {
-      fontFamily: "Yekan",
-      padding: "1em",
-      backgroundColor: "pink",
-    };
+    // const styles = {
+    //   textAlign: "center",
+    // };
 
     let person = null;
 
+    let badgeStyle = [];
+    if (persons.length >= 3) badgeStyle.push("badge-success");
+    if (persons.length <= 2) badgeStyle.push("badge-warning");
+    if (persons.length <= 1) badgeStyle.push("badge-danger");
+
     if (showPersons) {
       person = (
-        <Persons persons={persons} personDelet={this.handleDeletPerson}
-        personChange ={this.handleNameChange} />
+        <Persons
+          persons={persons}
+          personDelet={this.handleDeletPerson}
+          personChange={this.handleNameChange}
+        />
       );
     }
 
     return (
-      <div style={styles}>
-        <h2>مدیریت کننده اشخاص</h2>
-
-        <h4>.تعداد افراد {persons.length} نفر می باشد </h4>
+      <div className="rtl text-center">
+        <div className="alert alert-info">
+          <h2>مدیریت کننده اشخاص</h2>
+        </div>
+        <h5 className="alert alert-light">
+          تعداد افراد
+          <span className={`badge badge-pill  ${badgeStyle.join(" ")}`}>
+            {persons.length}
+          </span>
+          نفر می باشد
+        </h5>
         <hr />
-        {person}
-        <button onClick={this.handleShowPerson} style={buttonStyles}>
-          عدم/نمایش اشخاص
+        <div className="m-2 p-2">
+          <form
+            className="form-inline justify-content-center"
+            onSubmit={(evevnt) => evevnt.preventDefault()}
+          >
+            <div className="input-group w-25">
+              <input
+                type="text"
+                placeholder="ساخت شخص جدید"
+                className="form-control"
+                onChange={this.setPerson}
+                value={this.state.person}
+              />
+              <div className="input-group-prepend">
+                {" "}
+                <button
+                  type="submit"
+                  onClick={this.handleNewPerson}
+                  className="btn btn-success fa fa-plus-square"
+                />
+              </div>
+            </div>
+          </form>
+        </div>
+        <button
+          onClick={this.handleShowPerson}
+          className={showPersons ? "btn btn-info" : "btn btn-danger"}
+        >
+          نمایش اشخاص
         </button>
+        {person}
+        <ToastContainer />
       </div>
     );
   }
--- src/components/person/Person.jsx
+++ src/components/person/Person.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 
-import "./person.css";
 
 const Person = ({ fullname, personDelet, changed }) => {
   return (
-    <div className="person">
-      <p>{`${fullname}`}</p>
-      <input type="text" placeholder={fullname} onChange={changed} />
-      <button onClick={personDelet}>حذف </button>
+    <div className="card text-white bg-info mb-3 mt-3 w-25 mx-auto">
+        <div className="card-body text-center">
+          <p className="d-block">{`${fullname}`}</p>
+         <div className="input-group justify-content-center">
+             <input type="text" className="form-control w-50" placeholder={fullname} onChange={changed}/>
+              <div className="input-group-prepend">
+                 <button className="btn btn-sm btn-danger fa fa-trash" onClick={personDelet}/>
+
+         </div>
+        </div>
+      </div>
     </div>
   );
 };
