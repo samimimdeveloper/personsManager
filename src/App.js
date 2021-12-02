ccf88d04e163ba2709b6f7178405a766881b32b1
mahdi samimi <samimi.dev@gmail.com>
2021-12-03 01:23:58 +0330

    add style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import Persons from "./components/person/persons";
-import Person from './components/person/Person';
+import {Badge} from "antd"
+import 'antd/dist/antd.css'
 class App extends Component {
   state = {
     persons: [
       { id: 1, fullname: "حسین شجاعی " },
-      { id: 2, fullname: "یاسر زمانیان" },
-      { id: 3, fullname: " منصور نقی زاده" },
+      { id: 2, fullname: "علی قربانی" },
+      { id: 3, fullname: "مهران نظرزاده" },
     ],
 
     showPersons: false,
@@ -44,8 +45,10 @@ class App extends Component {
     const buttonStyles = {
       fontFamily: "Yekan",
       padding: "1em",
-      backgroundColor: "pink",
+      border:"unset",
+      cursor:"pointer"
     };
+    
 
     let person = null;
 
@@ -57,15 +60,15 @@ class App extends Component {
     }
 
     return (
-      <div style={styles}>
+      <div style={styles} >
         <h2>مدیریت کننده اشخاص</h2>
 
-        <h4>.تعداد افراد {persons.length} نفر می باشد </h4>
-        <hr />
-        {person}
+        <h4>.تعداد افراد <Badge count={persons.length  }   /> نفر می باشد </h4>
         <button onClick={this.handleShowPerson} style={buttonStyles}>
           عدم/نمایش اشخاص
         </button>
+        <hr />
+        {person}
       </div>
     );
   }
--- src/components/person/Person.jsx
+++ src/components/person/Person.jsx
@@ -5,9 +5,9 @@ import "./person.css";
 const Person = ({ fullname, personDelet, changed }) => {
   return (
     <div className="person">
-      <p>{`${fullname}`}</p>
-      <input type="text" placeholder={fullname} onChange={changed} />
-      <button onClick={personDelet}>حذف </button>
+      <p style={{textAlign:"center"}}>{`${fullname}`}</p>
+      <input style={{height:30,marginInline:8,width:400}} type="text" placeholder={fullname} onChange={changed} />
+      <button style={{border:"none",cursor:"pointer",height:30,width:60}} onClick={personDelet}>حذف </button>
     </div>
   );
 };
